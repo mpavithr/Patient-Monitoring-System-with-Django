cb38e8148451fe8df9ec54ddc6d2f244e6c4562c
Meenakshi Pavithran <42751267+mpavithr@users.noreply.github.com>
2022-03-26 03:09:57 -0400

    Update settings.py
    removed secret key because I am not sure if I can have it on a public repository

--- pms/settings.py
+++ pms/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u9sz_tn(d!1a3t9pced0)kxhx71jnt8wu3_$$$a+f+q-wow$lk'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
