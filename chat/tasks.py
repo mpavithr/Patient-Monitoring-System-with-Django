9f8ba080d2b25e804e5fc10ca5aa5d64b93e7f6f
Meenakshi <mpavithr@purdue.edu>
2022-03-30 18:26:55 -0400

    Added speech to text

--- chat/tasks.py
+++ chat/tasks.py
@@ -1,19 +1,23 @@
+import time
 import speech_recognition as sr
+
+# Phase 1:
+# def speech_to_text(speech_text):
+#     '''
+#     Convert speech to text using Google Speech Recognition
+#     '''
+
+#     time.sleep(10)
+#     print(speech_text)
 
 
 def speech_to_text(speech_file):
     '''
     Convert speech to text using Google Speech Recognition
     '''
-    # r = sr.Recognizer()
-    # with sr.AudioFile(speech_file) as source:
-    #     audio = r.record(source)
-    # try:
-    #     text = r.recognize_google(audio)
-    # except sr.UnknownValueError:
-    #     text = "Google Speech Recognition could not understand audio"
-    # except sr.RequestError as e:
-    #     text = "Could not request results from Google Speech Recognition service; {0}".format(
-    #         e)
-    # return text
-    print("Speech to text")
+
+    r = sr.Recognizer()
+    with sr.AudioFile(speech_file) as source:
+        audio_text = r.listen(source)
+        text = r.recognize_google(audio_text)
+        print(text)
